e3844780ee05dc1dd41ed7cf0068588b1a34c56d
woodyJo <jsungj3@gmail.com>
2024-01-26 14:31:42 +0900

    Test connect

---
+++ ShoesWeb/src/main/java/com/shoes/controller/Controller.java
@@ -0,0 +1,73 @@
+package com.shoes.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.shoes.command.SCommand;
+
+/**
+ * Servlet implementation class Controller
+ */
+@WebServlet("*.do")
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controller() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		actionDo(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		actionDo(request, response);
+	}
+	
+	public void actionDo (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("actionDo");
+		
+		HttpSession session = request.getSession();
+		
+		SCommand command = null;
+		String viewPage = null;
+		
+		String uri = request.getRequestURI();
+		String conPath = request.getContextPath();
+		String com = uri.substring(conPath.length());
+		
+		switch(com) {
+			case "list.do":
+				viewPage = "index.jsp";
+				break;
+			
+			default :
+				break;
+			
+		}
+		
+		
+		RequestDispatcher rd = request.getRequestDispatcher(viewPage);
+		rd.forward(request, response);	
+				
+	}
+
+
+}
---
+++ ShoesWeb/src/main/java/com/shoes/dto/LoginDto.java
@@ -0,0 +1,5 @@
+package com.shoes.dto;
+
+public class LoginDto {
+
+}
